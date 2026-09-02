4f5944a8fe68d70bd2a623611dba3d42d7e503d3
juquimuanga-lab <juquimuanga@gmail.com>
2026-09-02 22:26:22 +0200

    remove accidental main test file

--- backend/tests/test_hunt_sizes.py
+++
@@ -1,5 +0,0 @@
-from app.arbitrage.hunt import DEFAULT_HUNT_DISCOVERY_SIZES_SOL
-
-
-def test_default_hunt_discovery_sizes_include_040_sol():
-    assert DEFAULT_HUNT_DISCOVERY_SIZES_SOL == (0.02, 0.04, 0.10, 0.50)
